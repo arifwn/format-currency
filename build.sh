266e39208ba71ff1cf1a569402fb27fa637541ef
Arif Widi Nugroho <arif@sainsmograf.com>
2023-11-12 21:30:00 +0700

    updated build script

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 set -euxo pipefail
 
 echo "installing dependencies..."
-python -m pip install --upgrade pip build hatch
+python -m pip install --upgrade pip build hatch twine
+python -m build
+# python -m twine upload dist/*
+python -m twine upload --repository testpypi dist/*
 
 echo "done!"
