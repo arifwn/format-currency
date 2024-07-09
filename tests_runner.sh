6226a7c9c71e40c0cb36a575d596ec0854dc32ab
Arif Widi Nugroho <arif@sainsmograf.com>
2024-07-09 13:47:57 +0700

    bumped version number; updated test matrix to add python 3.12

--- tests_runner.sh
+++ tests_runner.sh
@@ -3,6 +3,11 @@
 set -euxo pipefail
 
 # python -m unittest discover
-hatch run test
+hatch test -py 3.7
+hatch test -py 3.8
+hatch test -py 3.9
+hatch test -py 3.10
+hatch test -py 3.11
+hatch test -py 3.12
 
 echo "done!"
